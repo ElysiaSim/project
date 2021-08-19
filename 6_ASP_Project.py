ef7ce1c09668ec97f27e6a955a6d57124be8668f
ocvb <electrokai1@gmail.com>
2021-08-19 10:54:23 +0800

    Update 6_ASP_Project.py

--- 6_ASP_Project.py
+++ 6_ASP_Project.py
@@ -2,13 +2,17 @@ import pandas as pd
 import numpy as np
 
 open = pd.read_excel("IMVA.xls", sheet_name=2)
-print(open)
 
 open1 = open["Periods"].str.split(" ", n = 1, expand = True)
 data = open.assign(Period= open1[0])
 data.index = data["Period"]
 del data["Periods"]
-print(data.columns)
+
+wafniwe = data[['Belgium & Luxembourg','Denmark',
+ 'Finland', 'France', 'Germany', 'Italy', 'Netherlands', 'Norway',
+ 'Rep Of Ireland', 'Russian Federation', 'Spain', 'Sweden', 'Switzerland',
+ 'United Kingdom']]
 
 data = data.loc[(data.Period >= str(1978)) & (data.Period <= str(1987))]
+print(len(wafniwe.columns))
 print(data)
