712d457c98873cbe2374df92f80754b8b2da0a3b
ocvb <electrokai1@gmail.com>
2021-08-19 11:09:24 +0800

    updates

--- 6_ASP_Project.py
+++ 6_ASP_Project.py
@@ -6,13 +6,16 @@ open = pd.read_excel("IMVA.xls", sheet_name=2)
 open1 = open["Periods"].str.split(" ", n = 1, expand = True)
 data = open.assign(Period= open1[0])
 data.index = data["Period"]
-del data["Periods"]
 
-wafniwe = data[['Belgium & Luxembourg','Denmark',
+data = data.loc[(data.Period >= str(1978)) & (data.Period <= str(1987))]
+
+selected_columns = data[['Belgium & Luxembourg','Denmark',
  'Finland', 'France', 'Germany', 'Italy', 'Netherlands', 'Norway',
  'Rep Of Ireland', 'Russian Federation', 'Spain', 'Sweden', 'Switzerland',
  'United Kingdom']]
 
-data = data.loc[(data.Period >= str(1978)) & (data.Period <= str(1987))]
-print(len(wafniwe.columns))
-print(data)
+print(data.head(3))
+print(selected_columns.columns)
+
+for name, values in selected_columns.iteritems():
+    print(np.sort('{name}: {value}'.format(name=name, value=values[0]), axis= None))
