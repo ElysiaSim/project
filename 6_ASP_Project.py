032fef89227b065aa7e288e05a6586b1acd380b1
ocvb <electrokai1@gmail.com>
2021-08-19 10:18:49 +0800

    aefen

---
+++ 6_ASP_Project.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+open = pd.read_excel("IMVA.xls", sheet_name=2)
+print(open)
+
+open1 = open["Periods"].str.split(" ", n = 1, expand = True)
+data = open.assign(Years= open1[0])
+data.index = data["Years"]
+del data["Periods"]
+print(data)
