78f85a20f15b8617cc52bb7d22cda3c551d7a671
ocvb <electrokai1@gmail.com>
2021-08-19 10:28:51 +0800

    ewjfnew

--- 6_ASP_Project.py
+++ 6_ASP_Project.py
@@ -8,4 +8,7 @@ open1 = open["Periods"].str.split(" ", n = 1, expand = True)
 data = open.assign(Years= open1[0])
 data.index = data["Years"]
 del data["Periods"]
+
+
+data = data.loc[(data.Years >= str(1978)) & (data.Years <= str(1987))]
 print(data)
