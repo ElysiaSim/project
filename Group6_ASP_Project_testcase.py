c38a26a85e078ca3e3fb1b97c45f928d314c68d8
ocvb <electrokai1@gmail.com>
2021-08-20 23:02:39 +0800

    UPDATE

--- Group6_ASP_Project_testcase.py
+++ Group6_ASP_Project_testcase.py
@@ -3,8 +3,7 @@ import Group6_ASP_Project as prog
 
 class test(unittest.TestCase):
     def test_mean(self):
-        self.assertEqual(round(prog.top3Mean, 2), 873071.33)
+        self.assertEqual(prog.top3Mean, 873071.33)
 
-    """
     def test_total(self):
-        self.assertEqual(prog.top3e, 2619214)"""
+        self.assertEqual(prog.top3e, 2619214)
